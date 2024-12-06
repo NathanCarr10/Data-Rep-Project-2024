cc016e07b9baf63c1e542bfc46af45bbde09b0d4
NathanCarr10 <[nathancarr18@gmail.com]>
2024-12-06 14:57:34 +0000

    created react app and tested by printing hello world + time in app.js file

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+    return (
+      <div>
+        <h1>Hello World!</h1>
+        <h2>It is {new Date().toLocaleTimeString()}.</h2>
+      </div>
+    );
+}
+
+export default App;
