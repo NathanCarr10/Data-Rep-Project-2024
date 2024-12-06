a55179a0be3d18e241593c30e2a6d7778b71ec63
NathanCarr10 <[nathancarr18@gmail.com]>
2024-12-06 15:06:52 +0000

    created Footer.js file

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+/**
+ * Footer component is a simple functional component
+ * that displays a footer message.
+ */
+
+const Footer = () => {
+    // Renders a heading (h3) containing the footer text
+      return <h3>My Footer in another component</h3>;
+    };
+    
+    export default Footer;
