d04f5d70ed8a301c5248ef56af3309fc2765ccd6
NathanCarr10 <[nathancarr18@gmail.com]>
2024-12-06 15:36:08 +0000

    installed axios

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
+//Importing necessary components from 'react-router-dom'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+//Importing Bootstrap CSS for styling the application
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+//Importing various custom components used in the app
 import NavigationBar from './components/NavigationBar';
-//import Header from './components/Header';
 import Footer from './components/Footer';
 import Content from './components/Content';
 
+//App component is the main component of the application.
 function App() {
   return (
+   //Router provides routing functionality to the entire application.
     <Router>
       <NavigationBar />
       <Routes>
-        <Route path="/home" element={<Content />} />
-        <Route path="/read" element={<h1>Read Component</h1>} />
-        <Route path="/create" element={<h1>Create Component</h1>} />
+        <Route path="/" element={<Content />} />
+        <Route path="/Read" element={<h1>Read Component</h1>} />
+        <Route path="/Create" element={<h1>Create Component</h1>} />
       </Routes>
       <Footer />
     </Router>
   );
 }
-
+//Exporting the App component to be rendered in the index.js file
 export default App;
