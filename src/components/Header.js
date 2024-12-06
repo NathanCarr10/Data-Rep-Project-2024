9509bf33b1745735c1da08ab5cf47fc6aa5936be
NathanCarr10 <[nathancarr18@gmail.com]>
2024-12-06 15:06:31 +0000

    created Header.js file

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+/**
+ * Header component is a simple functional component
+ * that displays a header message.
+ */
+
+const Header = () => {
+    // Renders a heading (h1) containing the header text
+    return <h1>My Header in another component</h1>;
+  };
+  
+  export default Header;
