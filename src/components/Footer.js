b1181ac749ef031fe5db228b96cc6c0a14c9787a
NathanCarr10 <[nathancarr18@gmail.com]>
2024-12-08 17:13:38 +0000

    Updated Footer component to always stay at the bottom + CSS styling

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,24 @@
+import React from "react";
+
 /**
  * Footer component is a simple functional component
  * that displays a footer message.
  */
 
 const Footer = () => {
-    // Renders a heading (h3) containing the footer text
-      return <h3>My Footer in another component</h3>;
+      return (
+        <footer style={{
+          backgroundColor: "#333",
+          color: "white",
+          textAlign:"center",
+          padding: "sticky",
+          bottom: 0,
+          width: "100%",
+        }}>
+          Your Personal Video Games Tracker!
+          <h2>It is {new Date().toLocaleTimeString()}.</h2>
+        </footer>
+      );
     };
     
     export default Footer;
